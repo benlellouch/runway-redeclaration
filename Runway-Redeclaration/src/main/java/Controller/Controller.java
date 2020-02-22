8ccb514ab33379512f3abd58a690e3c34f1497fd
Benjamin Lellouch <blellouch@gmail.com>
2020-02-22 16:47:17 +0000

    Added handling for file menu of main window

--- Runway-Redeclaration/src/main/java/Controller/Controller.java
+++ Runway-Redeclaration/src/main/java/Controller/Controller.java
@@ -1,6 +1,10 @@
 package Controller;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
 /**
  * @author Benjamin Lellouch
@@ -15,6 +19,72 @@ public class Controller {
      */
     public Controller()
     {
+
+    }
+
+    @FXML
+    public void defineAirport()
+    {
+        try
+        {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/AirportDefinition.fxml"));
+            loader.setControllerFactory(c ->
+                    this
+            );
+            Parent root = loader.load();
+            Stage definitionStage = new Stage();
+            Scene definitionScene = new Scene(root);
+            definitionStage.setScene(definitionScene);
+            definitionStage.show();
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+
+    }
+
+    @FXML
+    public void defineRunway()
+    {
+        try
+        {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/RunwayDefinition.fxml"));
+            Parent root = loader.load();
+            loader.setControllerFactory(c ->
+                    this
+            );
+            Stage definitionStage = new Stage();
+            Scene definitionScene = new Scene(root);
+            definitionStage.setScene(definitionScene);
+            definitionStage.show();
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+
+    }
+
+    @FXML
+    public void defineObstacle()
+    {
+        try
+        {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/ObstacleDefinition.fxml"));
+            Parent root = loader.load();
+            loader.setControllerFactory(c ->
+                    this
+            );
+            Stage definitionStage = new Stage();
+            Scene definitionScene = new Scene(root);
+            definitionStage.setScene(definitionScene);
+            definitionStage.show();
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
 
     }
 
