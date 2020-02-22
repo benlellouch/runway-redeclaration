f88b9c3cdd789a70505823730bf6857357c8ed77
Benjamin Lellouch <blellouch@gmail.com>
2020-02-22 16:27:01 +0000

    Created basic UI classes for the main window, obstacle definition, airport definition and runway definition with an Empty Controller

---
+++ Runway-Redeclaration/src/main/java/Controller/Controller.java
@@ -0,0 +1,23 @@
+package Controller;
+
+import javafx.fxml.FXML;
+
+/**
+ * @author Benjamin Lellouch
+ * The Controller will handle interactions between the back-end contained in the Model
+ * package and the front-end View and Resources packages.
+ */
+
+public class Controller {
+
+    /**
+     *  Controller Constructor
+     */
+    public Controller()
+    {
+
+    }
+
+
+
+}
--- Runway-Redeclaration/src/main/java/Main.java
+++ Runway-Redeclaration/src/main/java/Main.java
@@ -1,10 +1,27 @@
+import Controller.Controller;
+import com.sun.javafx.application.LauncherImpl;
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class Main extends  Application{
     @Override
     public void start(Stage primaryStage) throws Exception {
         primaryStage.setTitle("Runway Re-Declaration");
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/MainView.fxml"));
+        loader.setControllerFactory(c ->
+                new Controller()
+        );
+
+        Parent root = loader.load();
+        Scene primaryScene = new Scene(root);
+        primaryStage.setScene(primaryScene);
         primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        LauncherImpl.launchApplication(Main.class,args);
     }
 }
