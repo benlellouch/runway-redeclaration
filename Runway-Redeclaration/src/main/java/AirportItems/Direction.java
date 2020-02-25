9007661844c39c9b4de1c282c3475d153d9d9e77
db5n17 <db5n17@soton.ac.uk>
2020-02-25 21:22:02 +0000

    SEG-19: Completed the calculations algorithm.

---
+++ Runway-Redeclaration/src/main/java/AirportItems/Direction.java
@@ -0,0 +1,7 @@
+package AirportItems;
+
+public enum Direction
+{
+    LEFT,
+    RIGHT
+}
--- Runway-Redeclaration/src/main/java/AirportItems/LogicalRunway.java
+++ Runway-Redeclaration/src/main/java/AirportItems/LogicalRunway.java
@@ -1,23 +1,31 @@
 package AirportItems;
 
-public class LogicalRunway {
-
+public class LogicalRunway
+{
     private int lda;
     private int tora;
     private int toda;
     private int asda;
     private int displacedThreshold;
-    private String designator;
+    private int degree;
+    private Direction direction;
 
     public LogicalRunway(String designator, int tora, int toda, int asda, int lda, int displacedThreshold){
-        this.designator = designator;
         this.asda = asda;
         this.toda = toda;
         this.tora = tora;
         this.lda = lda;
         this.displacedThreshold = displacedThreshold;
+
+        degree = Integer.parseInt(designator.substring(0, 2)) * 10;
+        direction = (designator.substring(2, 3).equals("L")) ? Direction.LEFT : Direction.RIGHT;
     }
 
+    public String getName() { return degree + ((direction == Direction.LEFT) ? "L" : "R"); }
+
+    public int getDegree() { return degree; }
+
+    public Direction getDirection() { return direction; }
 
     public int getLda() {
         return lda;
@@ -35,7 +43,5 @@ public class LogicalRunway {
         return asda;
     }
 
-    public int getDisplacedThreshold() {
-        return displacedThreshold;
-    }
+    public int getDisplacedThreshold() { return displacedThreshold; }
 }
